3183cc5653a526d745ed55abc7c6d99545901a15
coolcatco888 <coolcatco888@c7892a96-3353-11de-9b0f-33ff132f608d>
2009-04-28 02:34:07 +0000

    Added with panel class, bug with panel image loading needs to be resolved.

---
+++ trunk/Prototypes/menutest/GameStateManagement/GameWindows/ScreenItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GameStateManagement.GameWindows
+{
+    /// <summary>
+    /// Represents graphical items in a panel, this could be either text or graphics
+    /// </summary>
+    abstract class ScreenItem
+    {
+        public abstract void Update(GameTime gameTime);
+
+        public abstract void Draw(GameTime gameTime, SpriteBatch spriteBatch);
+    }
+}
