db3a5cff73512b80b9ba6caee9edde5779bf22b0
coolcatco888 <coolcatco888@c7892a96-3353-11de-9b0f-33ff132f608d>
2009-04-28 22:07:40 +0000

    Last Commit did not add Fixed Menu Changes

---
+++ Prototypes/menutest/GameStateManagement/GameWindows/ImageComponent2D.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace GameStateManagement.GameWindows
+{
+    /// <summary>
+    /// Represents a 2D panel to be drawn on a screen. This implementation
+    /// is intended to be the base for menu windows, alert boxes, dialog boxes and HUDs.
+    /// This is essentially a collection of panel items.
+    /// </summary>
+    class ImageComponent2D : Component2D
+    {
+        private Texture2D image;
+
+        private Color tint;
+
+        public Texture2D Image
+        {
+            set { image = value; }
+            get { return image; }
+        }
+
+        public Color Tint
+        {
+            set { tint = value; }
+            get { return tint; }
+        }
+
+        /// <summary>
+        /// Creates a panel without an image, preferable for an HUD system
+        /// </summary>
+        public ImageComponent2D() : this (new Vector2(), null)
+        {
+        }
+
+        /// <summary>
+        /// Creates a panel with a background image, keep in mind panel items can be placed
+        /// outside of a menu but generally shouldn't.
+        /// </summary>
+        /// <param name="position"></param>
+        /// <param name="image"></param>
+        public ImageComponent2D(Vector2 position, Texture2D image) :  this (position, image, Color.White)
+        {
+        }
+
+
+        /// <summary>
+        /// Creates a panel with a background image with a tint color, keep in mind panel items can be placed
+        /// outside of a menu but generally shouldn't.
+        /// </summary>
+        /// <param name="position"></param>
+        /// <param name="image"></param>
+        /// <param name="tint"></param>
+
+        public ImageComponent2D(Vector2 position, Texture2D image, Color tint)
+        {
+            this.position = position;
+            this.image = image;
+            this.tint = tint;
+        }
+        
+        public override void Update(GameTime gameTime)
+        {
+
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+            spriteBatch.Draw(image, position, tint);
+            spriteBatch.End();
+        }
+    }
+}
+
---
+++ Prototypes/menutest/GameStateManagement/GameWindows/TextComponent2D.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GameStateManagement.GameWindows
+{
+    /// <summary>
+    /// Represents a text item in a panel
+    /// </summary>
+    class TextComponent2D : Component2D
+    {
+        #region Fields
+
+        private string text;
+
+        private Color color;
+
+        private SpriteFont font;
+
+        #endregion
+
+        #region Properties
+
+        public string Text 
+        { 
+            set { text = value; } 
+            get { return text; } 
+        }
+
+        public Color Color
+        {
+            set { color = value; }
+            get { return color; }
+        }
+
+        public SpriteFont Font
+        {
+            set { font = value; }
+            get { return font; }
+        }
+
+        #endregion
+
+        public TextComponent2D(string text, Color color, SpriteFont font, Vector2 position)
+        {
+            this.text = text;
+            this.color = color;
+            this.font = font;
+            this.position = position;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+            spriteBatch.DrawString(font, text, position, color);
+            spriteBatch.End();
+        }
+
+    }
+}
