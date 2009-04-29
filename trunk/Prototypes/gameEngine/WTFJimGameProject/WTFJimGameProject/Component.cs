9e4cbceb259fa7b05e97982222c05b734b08889a
jslemin <jslemin@c7892a96-3353-11de-9b0f-33ff132f608d>
2009-04-29 01:56:12 +0000

    Added a new folder with a very basic but working game engine.

---
+++ trunk/Prototypes/gameEngine/WTFJimGameProject/WTFJimGameProject/Component.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+namespace WTFJimGameProject
+{
+    public class BoundingRectangle
+    {
+        Vector2 topLeft;
+        Vector2 widthHeight;
+
+        public BoundingRectangle(float x, float y, float width, float height)
+        {
+            topLeft = new Vector2(x, y);
+            widthHeight = new Vector2(width, height);
+        }
+
+        public float Left
+        {
+            get { return topLeft.X; }
+        }
+
+        public float Right
+        {
+            get { return topLeft.X + widthHeight.X; }
+        }
+
+        public float Top
+        {
+            get { return topLeft.Y; }
+        }
+
+        public float Bottom
+        {
+            get { return topLeft.Y + widthHeight.Y; }
+        }
+    }
+
+    public interface I2DComponent
+    {
+        BoundingRectangle Bounds { get; set;}
+    }
+
+    public interface ITextComponent
+    {
+        //TODO: Add more parameters
+        String Text { get; set; }
+    }
+
+    public class Component
+    {
+        #region Parameters
+
+        public GameScreen Parent
+        {
+            get { return parent; }
+            set { parent = value; }
+        }
+        GameScreen parent;
+
+        public bool Initialized
+        {
+            get { return initialized; }
+            set { initialized = value; }
+        }
+        bool initialized;
+
+        public bool Enabled
+        {
+            get { return enabled; }
+            set { enabled = value; }
+        }
+        bool enabled;
+
+        public bool Visible
+        {
+            get { return visible; }
+            set { visible = value; }
+        }
+        bool visible;
+
+        #endregion
+
+        public Component(GameScreen parent)
+        {
+            this.parent = parent;
+            this.enabled = true;
+            this.visible = true;
+
+            Initialize(parent);
+        }
+
+        public virtual void Initialize(GameScreen parent)
+        {
+            //TODO: add component to parent
+            initialized = true;
+        }
+
+        public virtual void Update() { }
+
+        public virtual void Draw() { }
+
+        public virtual void Dispose()
+        {
+            parent.Components.Remove(this);
+        }
+    }
+}
---
+++ trunk/Prototypes/gameEngine/WTFJimGameProject/WTFJimGameProject/ScreenManager.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+namespace WTFJimGameProject
+{
+    public class ScreenManager : DrawableGameComponent
+    {
+        bool initialized;
+
+        public bool Initialized
+        {
+            get { return initialized; }
+            set { initialized = value; }
+        }
+
+        SpriteBatch spriteBatch;
+        SpriteFont spriteFont;
+
+        GameScreenCollection gameScreens;
+
+        public GameScreenCollection GameScreens
+        {
+            get { return gameScreens; }
+            set { gameScreens = value; }
+        }
+
+        List<GameScreen> updateList;
+        List<GameScreen> drawList;
+
+        public ScreenManager(Game game)
+            : base(game)
+        {
+            gameScreens = new GameScreenCollection();
+            updateList = new List<GameScreen>();
+            drawList = new List<GameScreen>();
+        }
+
+        public override void Initialize()
+        {
+            this.Enabled = true;
+            this.Visible = true;
+
+            initialized = true;
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(base.Game.GraphicsDevice);
+            base.LoadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+
+            // Populate the temp list
+            foreach (GameScreen screen in gameScreens)
+                if(screen.Enabled)
+                    updateList.Add(screen);
+
+            // BlocksUpdate and AlwaysUpdate login
+            for (int i = GameScreens.Count - 1; i >= 0; i--)
+                if (GameScreens[i].BlocksUpdate)
+                {
+                    if (i > 0)
+                        for (int j = i - 1; j >= 0; j--)
+                            if (!GameScreens[j].AlwaysUpdate)
+                                updateList.Remove(GameScreens[j]);
+
+                    break;
+                }
+
+            // Update remaining components
+            foreach (GameScreen screen in updateList)
+                if (screen.Initialized)
+                    screen.Update();
+
+            // Clear list
+            updateList.Clear();
+
+            base.Update(gameTime);
+
+        }
+
+        // Draws the current collection of screens and components. Accepts a
+        // ComponentType to render
+        public override void  Draw(GameTime gameTime)
+        {
+            // Clear the back buffer
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // Populate the temp list if the screen is visible
+            foreach (GameScreen screen in GameScreens)
+                if (screen.Visible)
+                    drawList.Add(screen);
+
+            // BlocksDraw and OverrideDrawBlocked logic
+            for (int i = GameScreens.Count - 1; i >= 0; i--)
+                if (GameScreens[i].BlocksDraw)
+                {
+                    if (i > 0)
+                        for (int j = i - 1; j >= 0; j--)
+                        {
+                            if (!GameScreens[j].AlwaysDraw)
+                                drawList.Remove(GameScreens[j]);
+                        }
+
+                    break;
+                }
+
+            // Draw the remaining screens
+            foreach (GameScreen screen in drawList)
+                if (screen.Initialized)
+                    screen.Draw();
+
+            drawList.Clear();
+
+            base.Draw(gameTime);
+        }
+
+    }
+}
