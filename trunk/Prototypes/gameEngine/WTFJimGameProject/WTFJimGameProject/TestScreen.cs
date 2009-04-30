a07db0cc304f5396d4b58b6a79224ae31a02a19e
coolcatco888 <coolcatco888@c7892a96-3353-11de-9b0f-33ff132f608d>
2009-04-30 00:45:49 +0000

    Menu components fully integrated with main project so Main menu now displays on TestScreen.cs with a menu that is built from an xml file.

--- trunk/Prototypes/gameEngine/WTFJimGameProject/WTFJimGameProject/TestScreen.cs
+++ trunk/Prototypes/gameEngine/WTFJimGameProject/WTFJimGameProject/TestScreen.cs
@@ -11,15 +11,58 @@ using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 using Microsoft.Xna.Framework.Net;
 using Microsoft.Xna.Framework.Storage;
+using WTFJimGameProject.GameWindows;
 
 namespace WTFJimGameProject
 {
     class TestScreen : GameScreen
     {
+        /// <summary>
+        /// Handles the loading and unloading of game content.
+        /// </summary>
+        ContentManager content;
+
+        PanelComponent2D panel;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        public override void LoadContent()
+        {
+            if (content == null)
+                content = new ContentManager(Owner.Game.Services, "Content");
+
+            //NOTE: This just builds the menupanel but there is no functionality yet.
+            XMLPanel2DBuilder componentBuilder = new XMLPanel2DBuilder(this, content, "MenuPanels\\mainpanel.xml");
+            panel = componentBuilder.Panel;
+        
+            base.LoadContent();
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            content.Dispose();
+        }
+
         public TestScreen(string name, ScreenManager owner)
             : base(name, owner)
         {
             Components.Add(new TestSquare(this));
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            panel.Update();
+        }
+
+        public override void Draw()
+        {
+            base.Draw();
+            panel.Draw();
         }
     }
 }
