bed68172a97e19c798b4c3a7feeca2d6635ffd45
alexcfontaine <alexcfontaine@c7892a96-3353-11de-9b0f-33ff132f608d>
2009-04-30 18:12:43 +0000

    Generate 3D terrain via a bitmap height map.

---
+++ trunk/Prototypes/Terrain/TerrainPipeline/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TerrainPipeline")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("BCIT")]
+[assembly: AssemblyProduct("TerrainPipeline")]
+[assembly: AssemblyCopyright("Copyright © BCIT 2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3f2c92f7-e07f-4952-aec8-d826962ed6b4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
