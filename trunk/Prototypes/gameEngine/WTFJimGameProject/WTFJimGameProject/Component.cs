5223fd00c269f709910c3e287e5513b9b18e8b62
jslemin <jslemin@c7892a96-3353-11de-9b0f-33ff132f608d>
2009-05-01 19:02:09 +0000

    Changed Component to use IDrawable Interface. DOES NOT COMPILE

--- trunk/Prototypes/gameEngine/WTFJimGameProject/WTFJimGameProject/Component.cs
+++ trunk/Prototypes/gameEngine/WTFJimGameProject/WTFJimGameProject/Component.cs
@@ -14,68 +14,10 @@ using Microsoft.Xna.Framework.Storage;
 namespace WTFJimGameProject
 {
 
-    //TODO: This may have to be deprecated use Rectangle class from C# library?
-    public class BoundingRectangle
-    {
-        Vector2 topLeft;
-        Vector2 widthHeight;
-
-        public BoundingRectangle(float x, float y, float width, float height)
-        {
-            topLeft = new Vector2(x, y);
-            widthHeight = new Vector2(width, height);
-        }
-
-        public float Left
-        {
-            get { return topLeft.X; }
-        }
-
-        public float Right
-        {
-            get { return topLeft.X + widthHeight.X; }
-        }
-
-        public float Top
-        {
-            get { return topLeft.Y; }
-        }
-
-        public float Bottom
-        {
-            get { return topLeft.Y + widthHeight.Y; }
-        }
-    }
-
-    public interface I2DComponent
-    {
-        //TODO - Decide whether this is needed
-        BoundingRectangle Bounds { get; set;}
-        Vector2 Position { get; set; }
-        SpriteBatch SpriteBatch { get; set; }
-
-        void Update();
-
-        void Draw();
-    }
-
-    public interface ITextComponent
-    {
-        //TODO: Add more parameters
-        String Text { get; set; }
-    }
-    //TODO - Should this be abstract?  (I don't see the reason why a Component should be instantiated.
-    //An abstract class does not have to have abstract methods)
     public class Component
     {
+        //These paramaters should be fine left alone
         #region Parameters
-
-        public GameScreen Parent
-        {
-            get { return parent; }
-            set { parent = value; }
-        }
-        GameScreen parent;
 
         public bool Initialized
         {
@@ -91,14 +33,17 @@ namespace WTFJimGameProject
         }
         bool enabled;
 
-        public bool Visible
-        {
-            get { return visible; }
-            set { visible = value; }
-        }
-        bool visible;
+        #endregion
+
+        //This should generally not be touched
+        #region attributes
+
+        GameScreen parent;
 
         #endregion
+
+        //Note there is no default constructor
+        #region Constructor
 
         public Component(GameScreen parent)
         {
@@ -109,19 +54,24 @@ namespace WTFJimGameProject
             Initialize(parent);
         }
 
+        #endregion
+
+        // Section has the 3 main methods that should be overridden
+        #region Methods
+
         public virtual void Initialize(GameScreen parent)
         {
             //TODO: add component to parent
             initialized = true;
         }
 
-        public virtual void Update() { }
-
-        public virtual void Draw() { }
+        public virtual void Update(GameTime gameTime) { }
 
         public virtual void Dispose()
         {
             parent.Components.Remove(this);
         }
+
+        #endregion
     }
 }
