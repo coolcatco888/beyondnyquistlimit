72c7c737dc5217317d56c0b4287c6531ce9b9871
coolcatco888 <coolcatco888@c7892a96-3353-11de-9b0f-33ff132f608d>
2009-05-01 19:47:00 +0000

    Branched a copy of Jim's engine for my use.

---
+++ trunk/Prototypes/menuBase/gameEngine/WTFJimGameProject/WTFJimGameProject/TestSquare.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+namespace WTFJimGameProject
+{
+    class TestSquare : Component
+    {
+        public TestSquare(GameScreen parent)
+            : base(parent)
+        {
+
+        }
+
+        public override void Draw()
+        {
+            Parent.Owner.GraphicsDevice.Clear(Color.DarkOliveGreen);
+            base.Draw();
+        }
+    }
+}
