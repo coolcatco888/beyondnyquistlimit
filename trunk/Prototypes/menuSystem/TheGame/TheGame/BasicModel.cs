1df4373491c4b4421b950456990a7a967afb687a
coolcatco888 <coolcatco888@c7892a96-3353-11de-9b0f-33ff132f608d>
2009-05-03 20:41:02 +0000

    Branched Jim's base code to work off of my menuSystem

---
+++ trunk/Prototypes/menuSystem/TheGame/TheGame/BasicModel.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+
+namespace TheGame
+{
+    class BasicModel : Component, IDrawableComponent, I3DComponent
+    {
+
+        #region IDrawableComponent Members
+
+        public void Draw(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            Camera camera = (Camera)GameEngine.Services.GetService(typeof(Camera));
+
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.EnableDefaultLighting();
+                    effect.PreferPerPixelLighting = true;
+
+                    effect.World = Matrix.CreateScale(scale) * Matrix.CreateFromQuaternion(rotation) * Matrix.CreateTranslation(position);
+                    effect.View = camera.View;
+                    effect.Projection = camera.Projection;
+                }
+
+                mesh.Draw();
+            }
+        }
+
+        public bool Visible
+        {
+            get
+            {
+                return visible;
+            }
+            set
+            {
+                visible = value;
+            }
+        }
+        bool visible;
+
+        #endregion
+
+        #region I3DComponent Members
+
+        public Microsoft.Xna.Framework.Vector3 Position
+        {
+            get
+            {
+                return position;
+            }
+            set
+            {
+                position = value;
+            }
+        }
+        Vector3 position;
+
+        public Microsoft.Xna.Framework.Quaternion Rotation
+        {
+            get
+            {
+                return rotation;
+            }
+            set
+            {
+                rotation = value;
+            }
+        }
+        Quaternion rotation;
+
+        public float Scale
+        {
+            get
+            {
+                return scale;
+            }
+            set
+            {
+                scale = value;
+            }
+        }
+        float scale;
+
+        #endregion
+
+        Model model;
+
+        public BasicModel(GameScreen parent, Model model)
+            : base(parent)
+        {
+            visible = true;
+            position = Vector3.Zero;
+            rotation = Quaternion.Identity;
+            scale = 1.0f;
+
+            this.Parent = parent;
+            this.model = model;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            Quaternion rot = Quaternion.CreateFromYawPitchRoll(0.001f * gameTime.ElapsedGameTime.Milliseconds, 0.0f, 0.0f);
+
+            rotation *= rot;
+
+            base.Update(gameTime);
+        }
+    }
+}
---
+++ trunk/Prototypes/menuSystem/TheGame/TheGame/TestComponent.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+namespace TheGame
+{
+    class TestComponent : Component, IDrawableComponent 
+    {
+        public TestComponent(GameScreen parent)
+            : base(parent)
+        {
+            visible = true;
+        }
+
+        #region IDrawableComponent Members
+
+        public void Draw(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            GameEngine.Graphics.Clear(Color.Brown);
+        }
+
+        public bool Visible
+        {
+            get
+            {
+                return visible;
+            }
+            set
+            {
+                visible = value;
+            }
+        }
+        bool visible;
+
+        #endregion
+    }
+}
