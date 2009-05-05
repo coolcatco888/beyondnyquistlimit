3adf21cb5ece8fdbbd7e479d2504636ca72e4488
mwilliams@vdldodgeball.ca <mwilliams@vdldodgeball.ca@c7892a96-3353-11de-9b0f-33ff132f608d>
2009-05-05 01:42:42 +0000

    Audio added to explosions. Sprite Sheet Knight.png implemented.

---
+++ trunk/Prototypes/audioManager/TerrainTest/TheGame/Billboard.cs
@@ -0,0 +1,136 @@
+﻿
+#region Using Statements
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+#endregion  // Using Statements
+
+namespace TheGame
+{
+    class Billboard : Component, IDrawableComponent
+    {
+        #region Fields
+
+        VertexDeclaration vertexDeclaration = new VertexDeclaration(
+                GameEngine.Graphics, VertexPositionTexture.VertexElements);
+
+        protected Texture2D texture2D;
+
+        /// <summary>
+        /// Current position of this billboard on the world map.
+        /// </summary>
+        protected Vector3 position;
+        public Vector3 Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        /// <summary>
+        /// Scale of the billboard.
+        /// </summary>
+        private float scale;
+        public float Scale
+        {
+            get { return scale; }
+            set { scale = value; }
+        }
+
+        /// <summary>
+        /// Width of a sprite.
+        /// </summary>
+        protected int spriteWidth;
+
+        /// <summary>
+        /// Height of a sprite.
+        /// </summary>
+        protected int spriteHeight;
+
+        /// <summary>
+        /// Number of pixels padding between sprites on sprite sheet.
+        /// </summary>
+        protected int spritePadding;
+
+        protected VertexPositionTexture[] vertices;
+
+        BasicEffect basicEffect;
+
+        #endregion  // Fields
+
+        public Billboard(GameScreen parent, Texture2D texture2D, int spriteWidth, int spriteHeight, int spritePadding)
+            : base(parent)
+        {
+            visible = true;
+
+            // Assign texture.
+            this.texture2D = texture2D;
+            this.spriteWidth = spriteWidth;
+            this.spriteHeight = spriteHeight;
+            this.spritePadding = spritePadding;
+
+            scale = 1.0f;
+            position = new Vector3(0.0f, 0.0f, 0.0f);
+
+            // Setup basic effect.
+            basicEffect = new BasicEffect(GameEngine.Graphics, null);
+            basicEffect.Texture = texture2D;
+            basicEffect.TextureEnabled = true;
+
+            // Four vertices to represet billboard.
+            vertices = new VertexPositionTexture[4];
+
+            // Assign position.
+            vertices[0].Position = new Vector3(1, 3, 0);
+            vertices[1].Position = new Vector3(-1, 3, 0);
+            vertices[2].Position = new Vector3(-1, -1, 0);
+            vertices[3].Position = new Vector3(1, -1, 0);
+
+            // Assign texture coordinates to vertices.
+            vertices[0].TextureCoordinate = new Vector2(0, 0);
+            vertices[1].TextureCoordinate = new Vector2(1, 0);
+            vertices[2].TextureCoordinate = new Vector2(1, 1);
+            vertices[3].TextureCoordinate = new Vector2(0, 1);
+        }
+
+        #region IDrawableComponent Members
+
+        public void Draw(GameTime gameTime)
+        {
+            Camera camera = (Camera)GameEngine.Services.GetService(typeof(Camera));
+
+            GameEngine.Graphics.RenderState.AlphaTestEnable = true;
+            GameEngine.Graphics.RenderState.AlphaFunction = CompareFunction.GreaterEqual;
+            GameEngine.Graphics.RenderState.ReferenceAlpha = 200;
+
+            //GameEngine.Graphics.RenderState.SourceBlend = Blend.SourceColor;
+
+
+            // Assign world, view, & projection matricies to basicEffect.
+            basicEffect.World = Matrix.CreateWorld(position, -camera.LookAt, Vector3.Up);
+            basicEffect.View = camera.View;
+            basicEffect.Projection = camera.Projection;
+
+            // Draw billboard.
+            basicEffect.Begin();
+            basicEffect.CurrentTechnique.Passes[0].Begin();
+
+            GameEngine.Graphics.VertexDeclaration = vertexDeclaration;
+            GameEngine.Graphics.DrawUserPrimitives(PrimitiveType.TriangleFan, vertices, 0, 2);
+
+            basicEffect.CurrentTechnique.Passes[0].End();
+            basicEffect.End();
+
+            GameEngine.Graphics.RenderState.AlphaTestEnable = false;
+        }
+
+        private bool visible;
+        public bool Visible
+        {
+            get { return visible; }
+            set { visible = value; }
+        }
+
+        #endregion
+    }
+}
