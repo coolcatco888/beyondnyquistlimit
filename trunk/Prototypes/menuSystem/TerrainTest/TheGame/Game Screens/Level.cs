b027193321d72332e2d17fee5427b99a87111066
coolcatco888 <coolcatco888@c7892a96-3353-11de-9b0f-33ff132f608d>
2009-05-06 02:22:33 +0000

    Added Weapon Menu to Level however still need to work out scaling issue.

--- trunk/Prototypes/menuSystem/TerrainTest/TheGame/Game Screens/Level.cs
+++ trunk/Prototypes/menuSystem/TerrainTest/TheGame/Game Screens/Level.cs
@@ -12,6 +12,7 @@ using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 using Microsoft.Xna.Framework.Net;
 using Microsoft.Xna.Framework.Storage;
+using TheGame.Components.Display;
 #endregion
 
 namespace TheGame
@@ -19,6 +20,15 @@ namespace TheGame
     class Level : GameScreen
     {
         #region Fields
+
+        private Texture2D item;
+
+        private Actor actor;
+
+        protected KeyboardDevice keyboardDevice = (KeyboardDevice)GameEngine.Services.GetService(typeof(KeyboardDevice));
+        protected GamepadDevice gamepadDevice = (GamepadDevice)GameEngine.Services.GetService(typeof(GamepadDevice));
+        private GameWeaponMenuPanel2D weaponPanel;
+
         public HeightMapInfo TerrainHeightMap
         {
             get
@@ -47,6 +57,11 @@ namespace TheGame
 
         #endregion
 
+        public override void LoadContent()
+        {
+            base.LoadContent();
+        }
+
         public Level(string name, string terrainFileName)
             : base(name)
         {
@@ -55,9 +70,62 @@ namespace TheGame
             levelMap = new Terrain(this, terrainFileName);
             terrainHeightMap = levelMap.HeightMapInfo;
 
-            Actor actor = new Actor(this, GameEngine.Content.Load<Texture2D>("theifWalkRun"), 64, 64, 1);
+            item = GameEngine.Content.Load<Texture2D>("itemsm");
+
+            actor = new Actor(this, GameEngine.Content.Load<Texture2D>("theifWalkRun"), 64, 64, 1);
             actor.Position = new Vector3(0.0f, 0.0f, -30.0f);
         }
 
+        public override void Update()
+        {
+            base.Update();
+
+            if (gamepadDevice.IsButtonDown(Buttons.RightShoulder))
+            {
+                if (weaponPanel == null)
+                {
+                    CreateWeaponPanel();
+                    Components.Add(weaponPanel);
+                }
+                if (weaponPanel != null)
+                {
+                    string selected = weaponPanel.SelectNewWeapon(gamepadDevice.LeftStickPosition);
+                }
+            }
+            else
+            {
+                if (weaponPanel != null)
+                {
+                    weaponPanel.KillMenu();
+                    weaponPanel = null;
+                }
+
+            }
+        }
+
+        private void CreateWeaponPanel()
+        {
+            weaponPanel = new GameWeaponMenuPanel2D(this, GetActorScreenCoordinates());
+            weaponPanel.AddWeapon("Sword1", new ImageComponent2D(this, Vector2.Zero, item));
+            weaponPanel.AddWeapon("Sword2", new ImageComponent2D(this, Vector2.Zero, item));
+            weaponPanel.AddWeapon("Sword3", new ImageComponent2D(this, Vector2.Zero, item));
+            weaponPanel.AddWeapon("Sword4", new ImageComponent2D(this, Vector2.Zero, item));
+            weaponPanel.UpdateItemPositions();
+        }
+
+
+        private Vector2 GetActorScreenCoordinates()
+        {
+            Camera camera = (Camera)GameEngine.Services.GetService(typeof(Camera));
+            
+
+            Vector3 actorPosition = new Vector3(actor.Position.X, actor.Position.Y, actor.Position.Z);
+            Vector3.Transform(actorPosition, camera.View * camera.Projection);
+
+            //TODO: Fix Scaling Issue
+            Vector2 coords = new Vector2(actorPosition.X * 10 + 375, actorPosition.Y * 10 + 375);
+
+            return coords;
+        }
     }
 }
