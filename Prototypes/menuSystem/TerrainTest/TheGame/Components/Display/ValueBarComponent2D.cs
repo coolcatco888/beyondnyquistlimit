898ad7f6ef8d11f03fc6e2895a837ba9d0177884
coolcatco888 <coolcatco888@c7892a96-3353-11de-9b0f-33ff132f608d>
2009-05-07 03:54:43 +0000

    Added sum tasty HUD and Input Stuffs. Ooohh de la creme.

---
+++ Prototypes/menuSystem/TerrainTest/TheGame/Components/Display/ValueBarComponent2D.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TheGame.Components.Display
+{
+    class ValueBarComponent2D : ImageComponent2D
+    {
+        private int maxValue, currentValue;
+
+        private Color damageColor;
+
+        private Vector2 horizontalScale;
+
+        public int MaxValue
+        {
+            set { maxValue = value; }
+            get { return maxValue; }
+        }
+
+        public int CurrentValue
+        {
+            set { currentValue = value; }
+            get { return currentValue; }
+        }
+
+        public Color DamageColor
+        {
+            set { damageColor = value; }
+            get { return damageColor; }
+        }
+
+        public ValueBarComponent2D(GameScreen parent, Vector2 position, Texture2D image, Color tint, int maxValue, Color damageColor) 
+            : base(parent, position, image, tint)
+        {
+            this.maxValue = maxValue;
+            this.currentValue = this.maxValue;
+            horizontalScale = new Vector2(0.0f, 1.0f);
+            this.damageColor = damageColor;
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            spriteBatch.Begin();
+            spriteBatch.Draw(image, position, null, tint, 0.0f, Vector2.Zero, horizontalScale, SpriteEffects.None, 0.0f);
+            spriteBatch.End();
+        }
+
+        public void IncreaseDecreaseBarValue(int value)
+        {
+            currentValue += value;
+            currentValue = currentValue > maxValue ? maxValue : currentValue < 0 ? 0 : currentValue;
+            horizontalScale = new Vector2(0.0f, currentValue / maxValue);
+        }
+        
+    }
+}
