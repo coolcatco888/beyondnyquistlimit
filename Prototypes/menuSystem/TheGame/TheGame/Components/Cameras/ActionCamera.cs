e91c8c57898d67306aa82094031ca0a990d49f91
coolcatco888 <coolcatco888@c7892a96-3353-11de-9b0f-33ff132f608d>
2009-05-12 01:04:37 +0000

    Created base For ActionCamera, still working out the mechanics of it.

---
+++ Prototypes/menuSystem/TheGame/TheGame/Components/Cameras/ActionCamera.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheGame.Components.Cameras
+{
+    class ActionCamera : Camera
+    {
+        ActionCamera(GameScreen parent)
+            : base(parent)
+        {
+        }
+    }
+}
