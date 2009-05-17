26521f6804943d8c9a98a3cd7b7a20831bf2de89
mwilliams@vdldodgeball.ca <mwilliams@vdldodgeball.ca@c7892a96-3353-11de-9b0f-33ff132f608d>
2009-05-17 07:09:06 +0000

    Refactored and integrated with latest base.

---
+++ trunk/Prototypes/mw/Unstable Base/TheGame/TheGame/Components/Billboards/GroundEffect.cs
@@ -0,0 +1,63 @@
+﻿
+#region Using Statements
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Library;
+
+#endregion  // Using Statements
+
+namespace TheGame
+{
+    public class GroundEffect : Billboard, IMoveable
+    {
+        #region Fields
+
+        #endregion  // Fields
+
+        #region Accessors
+        #endregion  // Accessors
+
+        #region Constructors
+
+        public GroundEffect(GameScreen parent, SpriteInfo spriteInfo)
+            : base(parent, spriteInfo.SpriteSheet)
+        {
+        }
+
+        #endregion  // Constructors
+
+        public override void Draw(GameTime gameTime)
+        {
+            Camera camera = (Camera)GameEngine.Services.GetService(typeof(Camera));
+
+            // Assign world, view, & projection matricies to basicEffect.
+            basicEffect.World = Matrix.CreateScale(scale.X) * Matrix.CreateRotationX(MathHelper.PiOver2) * Matrix.CreateRotationY(rotation) * Matrix.CreateTranslation(position);
+            basicEffect.View = camera.View;
+            basicEffect.Projection = camera.Projection;
+
+            GameEngine.Graphics.RenderState.AlphaBlendEnable = true;
+            GameEngine.Graphics.RenderState.SourceBlend = Blend.One;
+            GameEngine.Graphics.RenderState.DestinationBlend = Blend.One;
+            GameEngine.Graphics.RenderState.BlendFunction = BlendFunction.Add;
+
+            // Draw billboard.
+            basicEffect.Begin();
+            basicEffect.CurrentTechnique.Passes[0].Begin();
+
+            GameEngine.Graphics.VertexDeclaration = vertexDeclaration;
+            GameEngine.Graphics.DrawUserPrimitives(PrimitiveType.TriangleFan, vertices, 0, 2);
+
+            basicEffect.CurrentTechnique.Passes[0].End();
+            basicEffect.End();
+
+            GameEngine.Graphics.RenderState.AlphaBlendEnable = false;
+            GameEngine.Graphics.RenderState.AlphaTestEnable = false;
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+        }
+    }
+}
