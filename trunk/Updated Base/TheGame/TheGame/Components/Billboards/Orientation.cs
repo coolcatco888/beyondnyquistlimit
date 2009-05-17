83356610396c42c742dafe5f55dbcf59f832f5da
mwilliams@vdldodgeball.ca <mwilliams@vdldodgeball.ca@c7892a96-3353-11de-9b0f-33ff132f608d>
2009-05-17 07:13:25 +0000

    Updated Base.

---
+++ trunk/Updated Base/TheGame/TheGame/Components/Billboards/Orientation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheGame
+{
+    /// <summary>
+    /// Possible orientations of the actor.
+    /// </summary>
+    public enum Orientation
+    {
+        South,
+        North,
+        West,
+        Southwest,
+        Northwest,
+        East,
+        Southeast,
+        Northeast
+    }
+}
