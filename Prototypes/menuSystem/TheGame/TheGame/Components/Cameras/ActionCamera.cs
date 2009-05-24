931b1382d48e8261209a1dc84111ed9c63146300
coolcatco888 <coolcatco888@c7892a96-3353-11de-9b0f-33ff132f608d>
2009-05-24 01:48:04 +0000

    Updated ActionCamera CharacterSelectScreen under maintanence.

--- Prototypes/menuSystem/TheGame/TheGame/Components/Cameras/ActionCamera.cs
+++ Prototypes/menuSystem/TheGame/TheGame/Components/Cameras/ActionCamera.cs
@@ -189,6 +189,10 @@ namespace TheGame.Components.Cameras
             newPosition *= velocity * gameTime.ElapsedGameTime.Milliseconds;
             position += newPosition;
 
+            Vector3 dir = lookAt - position;
+
+            //Calculate Rotation
+            Rotation = Quaternion.CreateFromYawPitchRoll((float)Math.Atan2(dir.X, dir.Z), (float)Math.Atan2(dir.Z, dir.Y), 0.0f);
 
             //Push camera a bit to the side if the player is directly under the camera
             if (Math.Floor(position.X) == Math.Floor(lookAt.X) && Math.Floor(position.Z) == Math.Floor(lookAt.Z))
