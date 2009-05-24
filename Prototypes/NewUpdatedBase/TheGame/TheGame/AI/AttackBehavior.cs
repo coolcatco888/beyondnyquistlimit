00d1c8c66531f67abb4a229ebd09f58a0f144be7
coolcatco888 <coolcatco888@c7892a96-3353-11de-9b0f-33ff132f608d>
2009-05-24 12:31:37 +0000

    Merged Alex's stuff - AI and player, actor, and monster changes

---
+++ Prototypes/NewUpdatedBase/TheGame/TheGame/AI/AttackBehavior.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace TheGame
+{
+    public class AttackBehavior : Behavior
+    {
+        protected float attackRadius;
+        protected Player target;
+
+        public AttackBehavior(GameScreen parent, Monster monster, float attackRadius)
+            : base(parent, monster)
+        {
+            this.type = BehaviorType.Attack;
+            this.attackRadius = attackRadius;
+            updateTimeInterval = 5.0f;
+        }
+
+
+        public override void React(GameTime gameTime)
+        {
+            target = (Player)((Level)Parent).PlayerList.GetNearest(monster.Position);
+            float distance = Vector3.Distance(monster.Position, target.Position);
+            if (distance <= attackRadius)
+            {
+                reacted = true;
+                desireLevel = 120;
+            }
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            monster.State = Actor.ActorState.Attacking;
+            monster.Direction = Vector3.Normalize(target.Position - monster.Position);
+            monster.Direction = new Vector3(monster.Direction.X, monster.Direction.Y, monster.Direction.Z);
+            this.GetOrientationFromDirection();
+            monster.Speed = 0.0f;
+        }
+
+        public override void ResetTimeInterval()
+        {
+            currentTimeInterval = updateTimeInterval;
+        }
+
+
+        private void GetOrientation(Vector3 refPosition)
+        {
+            Vector3 relDirection = Vector3.Normalize(monster.Position - refPosition);
+
+            float dot = Vector3.Dot(monster.Direction, relDirection);
+            if (dot <= -1 && dot > -0.85f)
+            {
+                monster.Orientation = Orientation.South;
+            }
+            else if (dot <= -0.85f && dot > -0.50f)
+            {
+                if (monster.Direction.X < 0 || monster.Direction.Z < 0)
+                    monster.Orientation = Orientation.Southwest;
+                else
+                    monster.Orientation = Orientation.Southeast;
+            }
+            else if (dot >= -0.5f && dot < 0.5f)
+            {
+                if (monster.Direction.X < 0 || monster.Direction.Z < 0)
+                    monster.Orientation = Orientation.West;
+                else
+                    monster.Orientation = Orientation.East;
+            }
+            else if (dot >= 0.5f && dot < 0.85f)
+            {
+                if (monster.Direction.X < 0 || monster.Direction.Z < 0)
+                    monster.Orientation = Orientation.Northwest;
+                else
+                    monster.Orientation = Orientation.Northeast;
+            }
+            else
+            {
+                monster.Orientation = Orientation.North;
+            }
+        }
+    }
+}
